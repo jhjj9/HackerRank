b6bc98ea1b13960686b8fd13871ddf9cd831c383
PIYUSH <151519592+jhjj9@users.noreply.github.com>
2024-11-12 18:29:25 +0530

    Update Solution.java

--- Java/Introduction/Java Date and Time/Solution.java
+++ Java/Introduction/Java Date and Time/Solution.java
@@ -1,19 +1,46 @@
-import java.util.Scanner;
-import java.time.LocalDate;
+import java.io.*;
+import java.util.Calendar;
+import java.util.Locale;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1); // Calendar months are zero-based
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        return calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH).toUpperCase();
+    }
+}
 
 public class Solution {
-    
-    public static String getDay(String day, String month, String year) {
-        LocalDate date = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
-        return date.getDayOfWeek().name();
-    }
-    
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String month = in.next();
-        String day = in.next();
-        String year = in.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        System.out.println(getDay(day, month, year));
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+        int day = Integer.parseInt(firstMultipleInput[1]);
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
